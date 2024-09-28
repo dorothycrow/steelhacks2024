0e49194bc2a900229dfb7043cff66d407f299419
rachelbuege <121844475+rachelbuege@users.noreply.github.com>
2024-09-28 14:46:34 -0400

    countdown

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-axios.get('https://www.googleapis.com/civicinfo/v2/elections')
+axios.get('https://www.googleapis.com/civicinfo/v2/elections?key=AIzaSyBOx-4M4gD-2KcQZkHqP0wVEfHOqVrv4nY')
   .then(response => {
     // Handle the successful response
-    // console.log(response.data);
+    console.log(response.data);
   })
   .catch(error => {
     // Handle any errors
