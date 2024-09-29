eb49fc7e616bead4708bb58babb0530ae88a69db
Riya Shah <112494259+ras425@users.noreply.github.com>
2024-09-29 11:42:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -141,8 +141,8 @@ def login():
 
 
 def get_upcoming_elections():
-    #put api key
-    API_KEY = 'AIzaSyBOx-4M4gD-2KcQZkHqP0wVEfHOqVrv4nY'
+    #put google civic api key
+    API_KEY = ''
     user = User.query.order_by(User.id.desc()).first()  # Get the last user added
     address = user.address + ' ' + user.city + ' ' + user.state
     #election_id = 9000
